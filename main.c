8c06c22e2ece8ac8de9537d1a1c5010095fe289a
fer <fer@Freyja>
2016-10-09 00:36:51 -0700

    Genesis

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * Hecho por||Coded by||Laved af||プログラムした作: 
+ * Fernanda Meza
+ * https://github.com/ferhammaren
+ */
+
+/* 
+ * File:   main.c
+ * Author: fer
+ *
+ * Created on October 9, 2016, 12:35 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
